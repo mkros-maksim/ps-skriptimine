8dc3d75b20d96bf5bf57fc531f143c4970eb940a
maksim <maksim.miruskov@voco.ee>
2024-11-11 08:52:28 +0000

    Practice1 - Using ArrayList Collection Add method

--- pr1.ps1
+++ pr1.ps1
@@ -3,12 +3,12 @@ $a1 = @(1, 2, 3)
 $a2 = @(4, 5, 6)
 
 #define empty array
-$a3 = @()
+$a3 = New-Object -TypeName 'System.Collections.ArrayList' ## $a3 = [System.Collections.ArrayList]@()
 
 #add new values to arrays
-$a3 += $a1[0] + $a2[0]
-$a3 += $a1[1] + $a2[1]
-$a3 += $a1[2] + $a2[2]
+$a3.Add($a1[0] + $a2[0]) | Out-Null
+$a3.Add($a1[1] + $a2[1]) | Out-Null
+$a3.Add($a1[2] + $a2[2]) | Out-Null
 
 #echo array3
 $a3
