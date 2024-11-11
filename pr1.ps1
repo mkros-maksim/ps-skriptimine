f19dae28f8538b0704518c0e68543b47c677c3bf
maksim <maksim.miruskov@voco.ee>
2024-11-11 08:43:55 +0000

    Arrays +

---
+++ pr1.ps1
@@ -0,0 +1,14 @@
+﻿#define two arrays
+$a1 = @(1, 2, 3)
+$a2 = @(4, 5, 6)
+
+#define empty array
+$a3 = @()
+
+#add new values to arrays
+$a3 += $a1[0] + $a2[0]
+$a3 += $a1[1] + $a2[1]
+$a3 += $a1[2] + $a2[2]
+
+#echo array3
+$a3
