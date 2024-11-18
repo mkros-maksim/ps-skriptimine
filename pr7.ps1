24652d80cb6f40f6d680c9e7d28b6c2a4ff4801c
maksim <maksim.miruskov@voco.ee>
2024-11-18 09:08:59 +0000

    randomly assigned a group out of 4 colors

---
+++ pr7.ps1
@@ -0,0 +1,16 @@
+﻿$colors = "Red", "Green", "Yellow", "Blue"
+
+
+$students = 1..20
+
+
+$students | ForEach-Object {
+    
+    $randomColor = $colors | Get-Random
+
+    
+    [PSCustomObject]@{
+        RollNumber = $_
+        Group      = $randomColor
+    }
+} | Format-Table -Property RollNumber, Group
