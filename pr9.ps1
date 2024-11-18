bde7a8d351faf88892c6eaedfab5701f15ab8380
maksim <maksim.miruskov@voco.ee>
2024-11-18 09:37:41 +0000

    ‘do while’ loop which should display “Notepad is running” but an interval of 1 second. Once all the instances have been closed, it should stop displaying that “Notepad is running”. At the end, it should also show the number of times, the statement was displayed.

---
+++ pr9.ps1
@@ -0,0 +1,17 @@
+﻿$displayCount = 0
+
+
+do {
+    $notepadProcesses = Get-Process notepad -ErrorAction SilentlyContinue
+   
+    if ($notepadProcesses) {
+        $displayCount++
+        
+        Write-Host "Notepad is running"
+    }
+    
+    Start-Sleep -Seconds 1
+    
+} while ($notepadProcesses)  
+
+Write-Host "The message was displayed $displayCount times."
