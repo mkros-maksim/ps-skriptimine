738bf2b136adc7597682a00e0e826af43c59ea80
maksim <maksim.miruskov@voco.ee>
2024-11-18 08:20:36 +0000

    Ask the user for 2 values and then compare them and display

---
+++ pr5.2.ps1
@@ -0,0 +1,7 @@
+﻿$value1 = Read-Host "Enter the first value"
+$value2 = Read-Host "Enter the second value"
+if($value1 -gt $value2){
+Write-Host "The higher number is : "$value1
+}else{
+Write-Host "The higher number is : "$value2
+}
