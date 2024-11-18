e703920aeb08395ed1061571856de3c4e8f683ed
maksim <maksim.miruskov@voco.ee>
2024-11-18 08:19:50 +0000

    Display the menu as below and ask the user to select the country.

---
+++ pr5.ps1
@@ -0,0 +1,23 @@
+﻿Write-Host 'Select Country' -ForegroundColor Yellow
+Write-Host '1: India' -ForegroundColor Cyan
+Write-Host '2: Australia' -ForegroundColor Cyan
+Write-Host '3: China' -ForegroundColor Cyan
+
+$choice = Read-Host 'Please select country'
+
+if($choice -eq '1')
+  {
+    Write-Host 'Indias capital is New Delhi' -ForegroundColor Green
+  }
+elseif($choice -eq '2')
+  {
+    Write-Host 'Australias capital is Canberra' -ForegroundColor Green
+  }
+elseif($choice -eq '3')
+  {
+    Write-Host 'China capital is Beijing' -ForegroundColor Green
+  }
+else
+  {
+    Write-Host 'Entered value is wrong' -ForegroundColor Red
+  }
