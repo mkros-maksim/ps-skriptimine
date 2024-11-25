45fb66d3f973a8d5a15726e64d1e0d9431a494e5
maksim <maksim.miruskov@voco.ee>
2024-11-25 08:45:02 +0000

    pr5 part2

---
+++ pr11.1.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem -Path C:\Temp\Test
+
+$result = Get-ChildItem -Path C:\Temp\Test -Include *.csv | Select Name, Length
+
+Write-Host
+Write-Host 'Filename': $result.Name
+Write-Host 'Size in KB': $($result.Length/1KB)
+Write-Host 'Size in MB': $($result.Length/1MB)
