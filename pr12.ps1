e0d9877c61e6c9451eacccf9ca476559dd80a90d
maksim <maksim.miruskov@voco.ee>
2024-11-25 09:06:14 +0000

    Ask user to first enter 2 numbers and then publish a menu

---
+++ pr12.ps1
@@ -0,0 +1,29 @@
+﻿$a = Read-Host "Enter first number: "
+$b = Read-Host "Enter second number: "
+Write-Host "Calculator" -ForegroundColor Green
+Write-Host "1: Addition" -ForegroundColor Yellow
+Write-Host "2: Subtraction" -ForegroundColor Yellow
+Write-Host "3: Division" -ForegroundColor Yellow
+Write-Host "4: Multiplication" -ForegroundColor Yellow
+$choice = Read-Host "Please enter your choice"
+switch($choice)
+{
+1
+{
+$([int]$a + [int]$b)
+}
+2
+{
+$([int]$a - [int]$b)
+}
+3
+{
+$([int]$a / [int]$b)
+}
+4
+{
+$([int]$a * [int]$b)
+}
+
+}
+
