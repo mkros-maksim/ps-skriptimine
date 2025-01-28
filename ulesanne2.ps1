bc1f560f29eee1f4578fe95eabc5b5ebbcb1c460
maksim <maksim.miruskov@voco.ee>
2025-01-28 09:13:59 +0000

    Lokaalsete kasutajate kustutamine süsteemist

---
+++ ulesanne2.ps1
@@ -0,0 +1,24 @@
+﻿
+$firstName = Read-Host "Sisesta oma eesnimi"
+$lastName = Read-Host "Sisesta oma perenimi"
+
+$userName = ($firstName + "." + $lastName).ToLower()
+
+try {
+    $user = Get-LocalUser -Name $userName -ErrorAction Stop
+
+    Remove-LocalUser -Name $userName -ErrorAction Stop
+    Write-Host "Kasutaja '$userName' on edukalt kustutatud."
+} catch {
+    if ($_.Exception.Message -match "The term 'Get-LocalUser' is not recognized") {
+        Write-Host "Kasutaja '$userName' ei eksisteeri või andmed ei ole õiges vormingus."
+    } else {
+        Write-Host "Kasutaja kustutamine ebaõnnestus: $($_.Exception.Message)"
+    }
+}
+
+if ($?) {
+    Write-Host "Käsk õnnestus."
+} else {
+    Write-Host "Käsk ebaõnnestus."
+}
